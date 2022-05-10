c043d6a1c3160682d3ce535d5e5424001dcc9b80
Juhi Sahu <juhisahu2023@gmail.com>
2022-05-10 14:23:20 +0530

    signup and login form with email via authO added

--- app.js
+++ app.js
@@ -2,19 +2,37 @@ const express = require("express");
 const ejs = require('ejs');
 const app= express();
 const path = require("path");
+const bodyparser= require("body-parser");
+const flash = require('connect-flash');
+const db = require("./configs/DBconnection");
+const formRoutes = require("./routers/form");
+
 //middlewares
 app.set('view engine', 'ejs');
+
+//initialize app
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+app.use(flash());
 
 //path
 const static_path= path.join(__dirname, "public");
 app.use(express.static(static_path));
 
 
+//db connection
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 //routing
 app.get('/', function (req, res) {
     res.render('index');
 })
 
+//routers
+app.use("/", formRoutes);
 
 //port
 const port = process.env.PORT || 4000;
---
+++ controller/passportLocal.js
@@ -0,0 +1,52 @@
+//Authentication Strategy
+const bcrypt = require('bcryptjs');
+const db = require("../configs/DBconnection");
+var localStrategy = require('passport-local').Strategy;
+
+module.exports = function (passport) {
+
+    // LOCAL LOGIN 
+    passport.use('local-login', new localStrategy({
+            usernameField : 'email',
+            passwordField : 'password',
+            passReqToCallback : true 
+        },
+        function(req, email, password, done) { 
+            email = email.toLowerCase();
+            db.query("SELECT * FROM users WHERE email = ?",[email], function(err, result){
+                if (err)
+                    return done(err);
+                if (!result.length) {
+                    console.log('No user found.');
+                    return done(null, false, {message: 'No user found.'}); 
+                }
+
+                // if the user is found but the password is wrong
+                if (!bcrypt.compareSync(password, result[0].password))
+                    return done(null, false, {message: 'Oops! Wrong password.'}); // create the loginMessage and save it to session as flashdata
+
+                // all is well, return successful user
+                return done(null, result[0]);
+            });
+        }
+    )
+)
+    // passport session setup required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        console.log(user)
+        done(null, user.email);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(email, done) {
+        db.query("SELECT * FROM users WHERE email = ? ",[email], function(err, result){
+            done(err, result[0]);
+
+        });
+    });
+
+}
+//End of Authentication Strategy
+
